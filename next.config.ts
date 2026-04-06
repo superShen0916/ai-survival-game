7c21ddb39a21a441f4f6bdab1f561369e0f08b95
shenpeng.sp0916 <shenpeng.sp0916@bytedance.com>
2026-04-06 15:13:37 +0800

    config: support github pages deployment
    Change-Id: I155ff551a17d7d9be862f1c476a3dd5c19cfcd88

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo =
+    process.env.GITHUB_REPOSITORY || "superShen0916/ai-survival-game";
+  assetPrefix = `/${repo.split("/")[1]}/`;
+  basePath = `/${repo.split("/")[1]}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  assetPrefix,
+  basePath,
   images: {
     unoptimized: true,
   },
